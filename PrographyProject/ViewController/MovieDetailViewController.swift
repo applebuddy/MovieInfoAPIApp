650a920f7c64102d428cdada731851a4e2672281
MinKyeongTae <kyeongtea92@gmail.com>
2019-08-29 20:47:53 +0900

    Add MainViewController Interface

---
+++ PrographyProject/ViewController/MovieDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MovieDetailViewController.swift
+//  PrographyProject
+//
+//  Created by MinKyeongTae on 29/08/2019.
+//  Copyright © 2019 MinKyeongTae. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+}
---
+++ PrographyProject/ViewController/MovieListViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MovieListViewController.swift
+//  PrographyProject
+//
+//  Created by MinKyeongTae on 29/08/2019.
+//  Copyright © 2019 MinKyeongTae. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    /*
+     // MARK: - Navigation
+
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+         // Get the new view controller using segue.destination.
+         // Pass the selected object to the new view controller.
+     }
+     */
+}
